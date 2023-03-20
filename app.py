67189e416ecc9efe91c732cbebae6df9d904b2ba
Shannon Bentley <bentley.shannon@yahoo.com>
2023-03-19 21:21:24 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+#Data Structure: Upper and Lower case alphabet in tuple
+import string
+
+alphabet_lower = tuple(string.ascii_lowercase)
+alphabet_upper = tuple(string.ascii_uppercase)
+
+#Data Structure: Numbers in Tuple
+numbers = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+
+#Data Structure: Special Characters in tuple
+special_char = ("!", "@", "#", "$", "%", "&")
+
+#Code Functionality
+user_name = input('Create a username: ')
+print('You successfully registered a username!')
+
+user_password = input('\nCreate a new password: ')
+print('You successfully created a new password!')
