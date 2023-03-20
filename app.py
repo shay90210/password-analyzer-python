8c171cc13a0f169bb4a8d3b55f13947cbc07a039
Shannon Bentley <bentley.shannon@yahoo.com>
2023-03-20 15:57:08 -0500

    update

--- app.py
+++ app.py
@@ -10,10 +10,11 @@ numbers = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
 # Data Structure: Special Characters in tuple
 special_char = ("!", "@", "#", "$", "%", "&")
 
-# Code Functionality
+# Beginning Code Functionality
 user_name = input('Create a username: ')
 print('You successfully registered a username!')
 
+# While loop check the password
 while True:
     user_password = input('\nCreate a new password: ')
 
