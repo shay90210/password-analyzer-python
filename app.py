61081fa74bb8a6269bc3814f6f09e6d675fc1f2b
Shannon Bentley <bentley.shannon@yahoo.com>
2023-03-22 00:01:05 -0500

    updated readme

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ alphabet_lower = tuple(string.ascii_lowercase)
 alphabet_upper = tuple(string.ascii_uppercase)
 
 # Data Structure: Numbers in Tuple
-numbers = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+numbers = tuple(string.digits)
 
 # Data Structure: Special Characters in tuple
 special_char = ("!", "@", "#", "$", "%", "&")
